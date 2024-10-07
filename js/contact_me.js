e3402948234a59f80fa20d56274b6f842f0e6912
DangNguyen <dangquang.1993@gmail.com>
2024-10-07 15:56:46 -0400

    Contact us

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "https://script.google.com/macros/s/AKfycbx3tHT9ED861b6BrXyLt0kW4BIy43Ec6TnyeLqw4Q_XykOJzA6XKphzS4isUa0aoQs/exec",
                 type: "POST",
                 data: {
                     name: name,
@@ -33,7 +33,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Thank you, " + firstName + ", for contacting us! We will get back to you soon! </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -47,8 +47,6 @@ $(function() {
                         .append("</button>");
                     $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
                     $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
                 },
             })
         },
